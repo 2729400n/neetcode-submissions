9272a53c578b73fd898c28931395fe99108680fb
Kevin Nnaji <171568812+2729400n@users.noreply.github.com>
2026-05-18 13:29:00 +0100

    Add: max-consecutive-ones - submission-33

---
+++ Data Structures & Algorithms/max-consecutive-ones/submission-33.py
@@ -0,0 +1,31 @@
+class Solution:
+    '''
+    Solution to neet code easy 
+    '''
+    # Aim for speed and memory reduction
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        # current counter
+        c = 0
+
+        # best so far
+        b = 0
+
+        # Create and enter iterator context
+        for v in nums: # preform iterator step
+            c = c*v+v # we have a binary setup so it counts
+            # in the case where it isn't bin usint a test is available
+            # also we can sub and test
+
+            # [optimization] if statements are to bold
+            # removes speed by handling branch predictions removing branching
+
+            # I know everything fits in safe 32bit values so no weird python leb128 shenanigans
+            # is it a negative the b<c 
+            s=((b-c)>>32)&1
+
+            # use 1 as the if using multiple and sub for 
+            b = c*s+b*(s^1)
+
+        return b
+
+        
